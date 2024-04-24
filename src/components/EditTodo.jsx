492cf17513eba9fc27eba9031111fe688c64167f
nguyencongnguyen <nguyen.nguyencong@addy.vn>
2024-04-24 09:13:01 +0700

    jk

---
+++ src/components/EditTodo.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+const EditTodo = ({ close, job, updateJob }) => { // Sử dụng props job và updateJob
+  const [edit, setEdit] = useState(job); // Sử dụng giá trị job để khởi tạo state edit
+
+  const handleUpdate = () => {
+    updateJob(edit); // Gọi hàm cập nhật công việc với giá trị mới
+  };
+  return (
+    <div className="editform">
+      <div className="edit-task">
+        <h2>Edit Task</h2>
+        <input
+          value={edit.job}
+          onChange={(e) => {
+            setEdit(e.target.value);
+          }}
+          type="text"
+          placeholder="Nhập công việc mới"
+          className="edit-input"
+        />
+        <button className="edit-btn" onClick={handleUpdate}>Sửa</button>
+        <div className="closePopup">
+          <div className="close-btn" onClick={close}>
+            <IoCloseSharp />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditTodo;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,114 @@
+import React, { useState , useEffect } from "react";
+import EditTodo from "./EditTodo";
+import { FaCalendarCheck } from "react-icons/fa";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { MdOutlineEditCalendar } from "react-icons/md";
+
+const TodoForm = () => {
+  const [job, setJob] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  const [jobs, setJobs] = useState(() => {
+    const jobs = JSON.parse(localStorage.getItem("jobs"));
+    if (!jobs) return [];
+    else return jobs;
+  });
+
+  const handleSubmit = () => {
+    setJobs((prev) => {
+      const tasks = [...prev,  { task: job, completed: false }];
+      const save = localStorage.setItem("jobs", JSON.stringify(tasks));
+      return tasks;
+    });
+
+    setJob("");
+  };
+
+  const handleCloseExit = () => {
+    return setEdit(false);
+  };
+  const handleChangeTask = (index) => {
+     setEdit(true);
+     setEditIndex(index);
+  };
+
+  const handleComplete = (index) => {
+    const updatedJobs = [...jobs];
+    updatedJobs[index].completed = true; // Đánh dấu công việc đã hoàn thành
+    localStorage.setItem("jobs", JSON.stringify(updatedJobs));
+    setJobs(updatedJobs);
+  };
+
+  const handleDelete = (index) => {
+    let newJobs = [...jobs];
+    newJobs.splice(index, 1);
+
+    localStorage.setItem("jobs", JSON.stringify(newJobs));
+    setJobs(newJobs);
+  };
+
+  const handleUpdate = (updatedJob) => {
+    // Cập nhật công việc đã chỉnh sửa vào danh sách công việc
+    let updatedJobs = [...jobs];
+    updatedJobs[editIndex].task = updatedJob;
+
+    // Lưu danh sách công việc mới vào local storage và cập nhật state
+    localStorage.setItem("jobs", JSON.stringify(updatedJobs));
+    setJobs(updatedJobs);
+
+    // Đóng form chỉnh sửa
+    handleCloseExit();
+  };
+
+ 
+
+  return (
+    <div className="TodoForm">
+      {edit && <EditTodo 
+      close={handleCloseExit}
+      job={jobs[editIndex]} // Truyền công việc đang được chỉnh sửa vào form chỉnh sửa
+      updateJob={handleUpdate} // Truyền hàm cập nhật công việc vào form chỉnh sửa
+      />}
+      <input
+        value={job}
+        onChange={(e) => {
+          setJob(e.target.value);
+        }}
+        type="text"
+        className="todo-input"
+        placeholder="Nhập việc cần làm của bạn"
+      />
+      <button onClick={handleSubmit} className="todo-btn">
+        Thêm
+      </button>
+
+      <ul className="todo-list">
+        {jobs.map((job, index) => (
+          <li key={index} className={job.completed ? "completed" : ""}>
+            {job.task}
+            <div className="btns">
+            {!job.completed && (
+                <div
+                  className="complete-btn"
+                  onClick={() => handleComplete(index)}
+                >
+                  <FaCalendarCheck />
+                </div>
+              )}
+              <div className="delete-btn" onClick={() => handleDelete(index)}>
+                <RiDeleteBin5Fill />
+              </div>
+              <div
+                className="update-btn"
+                onClick={() => handleChangeTask(index)}
+              >
+                <MdOutlineEditCalendar />
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default TodoForm;
